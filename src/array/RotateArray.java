58c2509487eeb677d28628493e220b00cf6ace2c
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-06-02 12:55:34 +0530

    ArrayRotation

---
+++ src/array/RotateArray.java
@@ -0,0 +1,33 @@
+package array;
+
+public class RotateArray {
+    public static void rotateArrayNaive(int[] arr, int d){
+        int n = arr.length;
+
+        for (int i= 0; i< d;i++){
+            int first = arr[0];
+
+            for (int j = 0; j < n-1; j++) {
+                arr[j]=arr[j+1];                
+            }
+            arr[n-1] = first;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6};
+        int d = 1;
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+
+        }
+        System.out.println("\nafter rotation "+d+" times");
+        rotateArrayNaive(arr,d);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+
+        }
+    }
+}
