6bc974003df1a390836d1a286ec044c8d62cba71
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-06-03 12:29:50 +0530

    ArrayRotation optimal solution

--- src/array/RotateArray.java
+++ src/array/RotateArray.java
@@ -14,16 +14,34 @@ public class RotateArray {
         }
     }
 
+    public static void reverse(int[] arr, int first, int last){
+        while (first<last)
+        {
+            int temp = arr[first];
+            arr[first] =  arr[last];
+            arr[last] = temp;
+            first++;
+            last--;
+        }
+    }
+    public static void leftRotate(int[] arr, int d){
+        int n = arr.length;
+        d= d%n;
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+    }
+
     public static void main(String[] args) {
         int[] arr={1,2,3,4,5,6};
         int d = 1;
         
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" ");
-
         }
         System.out.println("\nafter rotation "+d+" times");
-        rotateArrayNaive(arr,d);
+        leftRotate(arr,d);
+//        rotateArrayNaive(arr,d);
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" ");
